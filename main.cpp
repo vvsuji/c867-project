b9d23c35f574ae60cdb417bf66120b1ee6f6a21b
vvsuji <suji.falcon@gmail.com>
2024-03-07 19:51:02 -0500

    finished

--- main.cpp
+++ main.cpp
@@ -42,6 +42,8 @@ int main() {
                         std::stoi(studentInfo[5]), std::stoi(studentInfo[6]), std::stoi(studentInfo[7]),
                         studentInfo[8]);
 
+// NOW TESTING THE PRINT FUNCTIONS
+
     // Printing all students
     classRoster.printAll();
 
